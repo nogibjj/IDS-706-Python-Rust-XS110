80a31e4d732ef6066d03df1aa4fe3761a3c15cbb
Xinyi Sheng <111578487+xs110@users.noreply.github.com>
2023-10-23 04:15:25 +0000

    rust

--- src/main.rs
+++ src/main.rs
@@ -26,9 +26,14 @@ fn read_operation() -> Result<()> {
     let mut stmt = conn.prepare("SELECT * FROM GroceryDB LIMIT 5;")?;
     let rows = stmt.query_map(params![], |row| {
         Ok((
-            row.get(0)?,
-            row.get(1)?,
-            row.get(2)?,
+            row.get::<usize, String>(0)?,
+            row.get::<usize, String>(1)?,
+            row.get::<usize, String>(2)?,
+            row.get::<usize, String>(3)?,
+            row.get::<usize, String>(4)?,
+            row.get::<usize, String>(5)?,
+            row.get::<usize, String>(6)?,
+            row.get::<usize, String>(7)?,
         ))
     })?;
 
@@ -40,25 +45,7 @@ fn read_operation() -> Result<()> {
     Ok(())
 }
 
-fn update_operation() -> Result<()> {
-    let conn = Connection::open("GroceryDB.db")?;
-    let data_to_insert = vec![
-        // your data here...
-        // example: ("general name", "count_products", ...),
-    ];
 
-    for data in &data_to_insert {
-        conn.execute(
-            "INSERT INTO GroceryDB (general_name, count_products, ingred_FPro, avg_FPro_products, 
-            avg_distance_root, ingred_normalization_term, semantic_tree_name, 
-            semantic_tree_node) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8)",
-            params![data.0, data.1, data.2, data.3, data.4, data.5, data.6, data.7],
-        )?;
-    }
-
-    println!("Record updated successfully!");
-    Ok(())
-}
 
 fn delete_operation() -> Result<()> {
     let conn = Connection::open("GroceryDB.db")?;
@@ -66,7 +53,57 @@ fn delete_operation() -> Result<()> {
     println!("Record deleted successfully!");
     Ok(())
 }
+fn update_operation() -> Result<()> {
+    let mut conn = Connection::open("GroceryDB.db")?;
 
+    let data_to_insert = vec![
+        (
+            "general name",
+            "count_products",
+            "ingred_FPro",
+            "avg_FPro_products",
+            "avg_distance_root",
+            "ingred_normalization_term",
+            "semantic_tree_name",
+            "semantic_tree_node"
+        ),
+        (
+            "arabica coffee",
+            "21",
+            "0.18903204038025467",
+            "0.2754401549508692",
+            "2.0476190476190474",
+            "15.16666666666667",
+            "",
+            ""
+        ),
+        (
+            "grape tomatoes",
+            "18",
+            "0.21119429773632484",
+            "0.4212998456790123",
+            "3.111111111111111",
+            "10.594047619047616",
+            "",
+            ""
+        )
+    ];
+
+    let tx = conn.transaction()?;
+    for data in &data_to_insert {
+        tx.execute(
+            "INSERT INTO GroceryDB 
+            (general_name, count_products, ingred_FPro, avg_FPro_products, 
+            avg_distance_root, ingred_normalization_term, semantic_tree_name, 
+            semantic_tree_node) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8)",
+            params![data.0, data.1, data.2, data.3, data.4, data.5, data.6, data.7],
+        )?;
+    }
+    tx.commit()?;
+
+    println!("Record updated successfully!");
+    Ok(())
+}
 fn main() {
     connect_to_database().unwrap();
     create_operation().unwrap();
